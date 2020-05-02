509576a8de5737d4340712482693224a92ddcf7c
yu-af <a.a1.yu@mail.ru>
2020-05-02 14:37:55 +0400

    added new items page

--- app/js/main.js
+++ app/js/main.js
@@ -11,7 +11,29 @@ $(function(){
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4
-    });  
+    }); 
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    }) 
+
+    $(".icon-th-list").on('click', function(){
+        $(".products__item").addClass('list'),
+        $(".icon-th-list").addClass('active'),
+        $('.icon-th').removeClass('active')
+
+    });
+
+    $('.icon-th').on('click', function(){
+        $('.products__item').removeClass('list'),
+        $('.icon-th').addClass('active'),
+        $('.icon-th-list').removeClass('active')
+    });
 
     var mixer = mixitup('.products__inner-box');
 });
